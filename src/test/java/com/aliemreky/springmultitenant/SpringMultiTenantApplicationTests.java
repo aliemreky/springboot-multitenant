eed69db7e4840a19585d02851bf77ce673f0bc46
aliemreky <aekaya@anadolu.edu.tr>
2024-01-03 23:14:07 +0300

    initial

---
+++ src/test/java/com/aliemreky/springmultitenant/SpringMultiTenantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliemreky.springmultitenant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultiTenantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
